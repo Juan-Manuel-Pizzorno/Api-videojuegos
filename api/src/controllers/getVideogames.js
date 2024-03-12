95be57935c58a38532354160de14a275a183eeda
juan-manuel-pizzorno <juan_19_2013@hotmail.com>
2024-03-12 01:03:02 -0300

    tratando de enviar los primeros 100 juegos al back

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -1,32 +1,37 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-const { Genres } = require('../db'); // Asegúrate de importar el modelo correcto
+const { Genres } = require("../db"); // Asegúrate de importar el modelo correcto
 
 const API_KEY = process.env.API_KEY;
-const URL = 'https://api.rawg.io/api/games';
+const URL = "https://api.rawg.io/api/games";
 
 const getVideogames = async (req, res) => {
   try {
     const response = await axios.get(`${URL}?key=${API_KEY}`);
-    
+
+    //Guardo en videogames lo que me traigo de la api, en este cado todos los videogames
+    //ya que en la API los results es donde estan los datos de c/ juego.
     const videogames = response.data.results;
 
+    //Aqui guardo la constante para ir a la siguiente página ya que cada página tiene 20 juegos solamente
+    const next = response.data.next;
+
     const videoArray = videogames.map((videogame) => {
-
-      const {id,name,background_image,genres} = videogame;
-
+      //Desestructuro los datos de la API que necesecito en cada item, tal cual viene de la Api
+      const { id, name, background_image, genres } = videogame;
+      //Realizo un mapeo para sacar los generos de cada juego ya que es una propiedad del objeto
       const genreNames = genres.map((genre) => genre.name).join(", ");
 
-    
-
       return {
-        id,name,background_image,genreNames
+        id,
+        name,
+        background_image,
+        genreNames, //luego con este nombre tengo que llarmo desde el front
       };
     });
-
+    console.log(videoArray.length);
     return res.status(200).json(videoArray);
-
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,44 +1,34 @@
-import { getVideogames, cleanDetail } from "../../redux/actions"
+import { getVideogames, cleanDetail } from "../../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-
+import { useEffect } from "react";
+import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Cards() {
-
   const dispatch = useDispatch();
   const videogame = useSelector((state) => state.videogames);
 
   useEffect(() => {
-    const videogames = async () => {
-
-      setTimeout(async () => {
-        await dispatch(getVideogames()); // Espera a que se complete la acción, sino aparece la imagen default
-        //setLoading(false); // Establece loading a false después de cargar los datos
-      },);
-      dispatch(cleanDetail()); // ver esto xq va al final creo
+    const fetchData = async () => {
+      await dispatch(getVideogames());
+      dispatch(cleanDetail());
     };
-    videogames();
+    fetchData();
   }, [dispatch]);
 
-
-  //const hola = videogame.map(videogames => videogames.name) //con esto saco el nombre de todos
-
   return (
-    <div>
-
-      {
-        videogame.map(videogame =>
-
-          <Row xs={1} md={2} className="g-4" key={videogame.id}>
-            {Array.from({ length: 1 }).map((_, idx) => (
+    
+    
+        
+          <Row xs={1} md={2} className="g-4" >
+            {videogame.map(videogame => (
               <Col key={videogame.id}>
-                <Card >
+                <Card key={videogame.id}>
                   <Card.Img variant="top" src={videogame.background_image} />
                   <Card.Body>
-                    <Card.Title> {videogame.name}  </Card.Title>
+                    <Card.Title>{videogame.name}</Card.Title>
                     <Card.Text>
                       {videogame.genreNames}
                     </Card.Text>
@@ -47,11 +37,5 @@ export default function Cards() {
               </Col>
             ))}
           </Row>
-        )}
-
-
-
-
-    </div>
-  )
+        )
 }
