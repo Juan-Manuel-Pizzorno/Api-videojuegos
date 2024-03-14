b8635621a954aa5fefc2897b8c2e5bf1f8e7d88f
juan-manuel-pizzorno <juan_19_2013@hotmail.com>
2024-03-14 00:52:03 -0300

    aplicando estilos a las CARDS al pasar el mouse, correcto funcionamiento al hacer click y que muestre DetailCard

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -12,19 +12,23 @@ let videogames = [];
 
 const getVideogames = async (req, res) => {
   try {
-    //Tengo que traer 100 juegos de la api
+    //Tengo que traer 100 juegos de la API asi que realizo un while xq traigo 20 por pagina nomás
     while (videoArray.length < 100 && URL) {
       let response = await axios.get(URL);
-      //Guardo en videogames lo que me traigo de la api, en este cado todos los videogames
-      //ya que en la API los results es donde estan los datos de c/ juego.
+      //desestructuro la response de la api en 2 partes: results donde estaran todos los datos de los juegos
+      //y en next, donde estará el dato para pasar a la siguiente página y trae los siguientes 20 juegos
+      //results y next son los nombres que tienen los valores en la página de la API
       const { results, next } = response.data;
-      console.log(results);
+
+      //Al array videoArray le guardo los datos de la API(results) a lo que tenia antes en el array
+      //videoArray(...videoArray)
       videoArray = [...videoArray, ...results];
 
       //Aqui guardo la constante para ir a la siguiente página ya que cada página tiene 20 juegos solamente
       URL = next;
     }
-
+    //luego de guardar toda la información en videoArray, desestructuro en partes el mismo y lo guardo en 
+    //otro array videogames
     videogames = videoArray.map((videogame) => {
       //Desestructuro los datos de la API que necesecito en cada item, tal cual viene de la Api
       const { id, name, background_image, genres } = videogame;
@@ -35,11 +39,11 @@ const getVideogames = async (req, res) => {
         id,
         name,
         background_image,
-        genreNames, //luego con este nombre tengo que llarmo desde el front
+        genreNames, //luego con este nombre tengo que llamarlo desde el front
       };
     });
-    console.log(videoArray.length);
-    return res.status(200).json(videoArray);
+    //console.log(videoArray.length);
+    return res.status(200).json(videogames);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,14 +1,37 @@
 import { getVideogames, cleanDetail } from "../../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import Container from 'react-bootstrap/Container';
+import { useEffect,useState} from "react";
+import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+/*Aquí se está utilizando la expresión ternaria videogame.id === hoveredCard ? hoverCardStyle : cardStyle para 
+determinar qué estilo aplicar. Si el ID del videojuego es igual al hoveredCard (es decir, si el mouse está sobre
+   esta tarjeta), se aplica el estilo hoverCardStyle, que incluye una sombra para simular el efecto de 
+   iluminación. Si el ID no es igual al hoveredCard, se aplica el estilo cardStyle, que es el estilo 
+   predeterminado sin sombra.
+
+Además, se han añadido los eventos onMouseEnter y onMouseLeave al componente Card para actualizar el estado 
+hoveredCard. Cuando el mouse entra en la tarjeta, se establece el estado hoveredCard con el ID del videojuego 
+correspondiente, y cuando el mouse sale de la tarjeta, se restablece el estado a null. Esto permite cambiar 
+dinámicamente los estilos cuando el mouse entra y sale de la tarjeta, creando el efecto de iluminación deseado.*/
+
+
+const cardStyle = {
+  height: '400px', // Ajusta la altura según tus necesidades
+  transition: 'box-shadow 0.3s', // Transición suave del efecto
+};
+
+const hoverCardStyle = {
+  ...cardStyle,
+  boxShadow: '0px 0px 10px 5px rgba(0,0,0,0.3)', // Agrega una sombra al pasar el mouse
+};
+
 export default function Cards() {
   const dispatch = useDispatch();
   const videogame = useSelector((state) => state.videogames);
+  const [hoveredCard, setHoveredCard] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,18 +43,24 @@ export default function Cards() {
 
   return (
 
-    <Row xs={1} md={2} className="g-4" >
+    <Row xs={3} md={4} className="g-5" >
       {videogame.map(videogame => (
         <Col key={videogame.id}>
-          <Card key={videogame.id}>
-            <Card.Img variant="top" src={videogame.background_image} />
-            <Card.Body>
-              <Card.Title>{videogame.name}</Card.Title>
-              <Card.Text>
-                {videogame.genreNames}
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <Link to={`/videogames/${videogame.id}`} style={{ textDecoration: 'none' }}>
+          <Card
+              style={videogame.id === hoveredCard ? hoverCardStyle : cardStyle}
+              onMouseEnter={() => setHoveredCard(videogame.id)}
+              onMouseLeave={() => setHoveredCard(null)}
+            >
+              <Card.Img variant="top" src={videogame.background_image} style={{height:"300px"}} />
+              <Card.Body>
+                <Card.Title>{videogame.name}</Card.Title>
+                <Card.Text>
+                  {videogame.genreNames}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
         </Col>
       ))}
     </Row>
